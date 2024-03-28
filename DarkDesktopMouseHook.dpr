a0ba26574c87b5eefb67c0fb189638468a46fe6d
vhanla <vhanla@gmail.com>
2024-03-28 12:53:44 -0500

    🎁 New files added, Mouse Hook DLL project

---
+++ DarkDesktopMouseHook.dpr
@@ -0,0 +1,195 @@
+library DarkDesktopMouseHook;
+
+{ Important note about DLL memory management: ShareMem must be the
+  first unit in your library's USES clause AND your project's (select
+  Project-View Source) USES clause if your DLL exports any procedures or
+  functions that pass strings as parameters or function results. This
+  applies to all strings passed to and from your DLL--even those that
+  are nested in records and classes. ShareMem is the interface unit to
+  the BORLNDMM.DLL shared memory manager, which must be deployed along
+  with your DLL. To avoid using BORLNDMM.DLL, pass string information
+  using PChar or ShortString parameters.
+
+  Important note about VCL usage: when this DLL will be implicitly
+  loaded and this DLL uses TWicImage / TImageCollection created in
+  any unit initialization section, then Vcl.WicImageInit must be
+  included into your library's USES clause. }
+
+uses
+  Windows,
+  Messages;
+
+const
+  MemMapFile = 'DarkDesktop';
+
+const
+  WM_KEYSTATUSCHANGED = WM_USER + 3;
+
+type
+  PKeyStatus = ^TKeyStatus;
+  TKeyStatus = record
+    ShiftPressed: Boolean;
+    CtrlPressed: Boolean;
+    AltPressed: Boolean;
+  end;
+
+type
+  PKBDLLHOOKSTRUCT = ^TKBDLLHOOKSTRUCT;
+  TKBDLLHOOKSTRUCT = record
+    vkCode: Cardinal;
+    scanCode: Cardinal;
+    flags: Cardinal;
+    time: Cardinal;
+    dwExtrainfo: Cardinal;
+  end;
+
+  PDLLGlobal = ^TDLLGlobal;
+  TDLLGlobal = packed record
+    HookHandle: HHOOK;
+    KeyHookHandle: HHOOK;
+    PrevKeyStatus: TKeyStatus;
+  end;
+
+var
+  GlobalData: PDLLGlobal;
+  MMF: THandle;
+
+{$R *.res}
+
+function HookProc(Code: Integer; wParam: WPARAM; lParam: LPARAM): LRESULT; stdcall;
+var
+//  CurWnd: THandle;
+  TargetWnd: THandle;
+  Msg: PCopyDataStruct;
+//  MsgStr: String;
+begin
+  //if Code < 0 then
+  if (Code < 0) or (Code = HC_NOREMOVE) then
+  begin
+    Result := CallNextHookEx(GlobalData^.HookHandle, Code, wParam, lParam);
+    Exit;
+  end;
+
+  if (wParam = WM_MOUSEMOVE) then
+  begin
+    TargetWnd := FindWindow('DarkDesktopClass', nil);
+    if TargetWnd <> 0 then
+    begin
+      New(Msg);
+      Msg^.dwData := 0;
+      Msg^.cbData := SizeOf(TMouseHookStruct);
+      Msg^.lpData := PMouseHookStruct(lParam);
+      SendMessageTimeout(TargetWnd, WM_COPYDATA, 0, Integer(Msg), SMTO_ABORTIFHUNG, 50, nil);
+      Dispose(Msg);
+    end;
+  end;
+
+  Result := CallNextHookEx(GlobalData^.HookHandle, Code, wParam, lParam);
+end;
+
+function KeyboardHookProc(nCode: Integer; wParam: WPARAM; lParam: LPARAM): LRESULT; stdcall;
+var
+  HookStruct: PKBDLLHOOKSTRUCT;
+  CurrentKeyStatus: TKeyStatus;
+begin
+  if nCode = HC_ACTION then
+  begin
+    HookStruct := PKBDLLHOOKSTRUCT(lParam);
+
+    // Check if any of the modifier keys (Ctrl, Alt, Shift) have changed
+    CurrentKeyStatus.CtrlPressed := HookStruct^.vkCode = VK_CONTROL;
+    CurrentKeyStatus.AltPressed := HookStruct^.vkCode = VK_MENU;
+    CurrentKeyStatus.ShiftPressed := HookStruct^.vkCode = VK_SHIFT;
+
+    if (CurrentKeyStatus.CtrlPressed <> GlobalData^.PrevKeyStatus.CtrlPressed)
+    or (CurrentKeyStatus.AltPressed <> GlobalData^.PrevKeyStatus.AltPressed)
+    or (CurrentKeyStatus.ShiftPressed <> GlobalData^.PrevKeyStatus.ShiftPressed)
+    then
+    begin
+      var TargetWnd := FindWindow('DarkDesktopClass', nil);
+      if TargetWnd <> 0 then
+      begin
+        // Send a message to the calling application with the updated key status
+        PostMessage(TargetWnd, WM_KEYSTATUSCHANGED, wParam, Windows.LPARAM(@CurrentKeyStatus));
+      end;
+
+      // Update the previous key status
+      GlobalData^.PrevKeyStatus := CurrentKeyStatus;
+    end;
+
+
+  end;
+
+  // Call the next hook in the chain
+  Result := CallNextHookEx(GlobalData^.KeyHookHandle, nCode, wParam, lParam);
+end;
+
+procedure CreateGlobalHeap;
+begin
+  MMF := CreateFileMapping(INVALID_HANDLE_VALUE, nil, PAGE_READWRITE, 0,
+    SizeOf(TDLLGlobal), MemMapFile);
+
+  if MMF = 0 then
+  begin
+    MessageBox(0, 'CreateFileMapping -', '', 0);
+    Exit;
+  end;
+
+  GlobalData := MapViewOfFile(MMF, FILE_MAP_ALL_ACCESS, 0, 0, SizeOf(TDLLGlobal));
+  if GlobalData = nil then
+  begin
+    CloseHandle(MMF);
+    MessageBox(0, 'MapViewFile -', '', 0);
+  end;
+end;
+
+procedure DeleteGlobalHeap;
+begin
+  if GlobalData <> nil then
+    UnmapViewOfFile(GlobalData);
+
+  if MMF <> INVALID_HANDLE_VALUE then
+    CloseHandle(MMF);
+end;
+
+procedure RunHook; stdcall;
+begin
+  GlobalData^.HookHandle := SetWindowsHookEx(WH_MOUSE_LL, @HookProc, HInstance, 0);
+  if GlobalData^.HookHandle = INVALID_HANDLE_VALUE then
+  begin
+    MessageBox(0, 'Error :)', '', MB_OK);
+    Exit;
+  end;
+  GlobalData^.KeyHookHandle := SetWindowsHookEx(WH_KEYBOARD_LL, @KeyboardHookProc, HInstance, 0);
+  if GlobalData^.KeyHookHandle = INVALID_HANDLE_VALUE then
+  begin
+    MessageBox(0, 'Error :)', '', MB_OK);
+    Exit;
+  end;
+end;
+
+procedure KillHook; stdcall;
+begin
+  if (GlobalData <> nil) and (GlobalData^.KeyHookHandle <> INVALID_HANDLE_VALUE) then
+    UnhookWindowsHookEx(GlobalData^.KeyHookHandle);
+  if (GlobalData <> nil) and (GlobalData^.HookHandle <> INVALID_HANDLE_VALUE) then
+    UnhookWindowsHookEx(GlobalData^.HookHandle);
+end;
+
+procedure DLLEntry(dwReason: DWORD);
+begin
+  case dwReason of
+    DLL_PROCESS_ATTACH: CreateGlobalHeap;
+    DLL_PROCESS_DETACH: DeleteGlobalHeap;
+  end;
+end;
+
+exports
+  KillHook,
+  RunHook;
+
+begin
+  DllProc := @DLLEntry;
+  DLLEntry(DLL_PROCESS_ATTACH);
+end.
+
